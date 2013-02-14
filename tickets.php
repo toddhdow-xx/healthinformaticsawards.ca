5a0ebb9305432a0383bbba09a0bf2252c2f2234e
Todd Dow <toddhdow@gmail.com>
2013-02-14 11:50:32 -0500

    The registration pages are being spammed. Caitlyn Smith directed me to remove this functionality from the site (in an email from 2013-02-01.

--- tickets.php
+++ tickets.php
@@ -12,20 +12,7 @@ include "body_header.php";
 <tr>
 <td class="b" colspan="2">
 <div style="padding: 0px 10px 0px 10px;">
-
-<p align="left">A limited number of individual tickets and tables of 10 can be purchased for the CHIA Gala 2012.</p>
-
-<p align="left">This elegant evening will feature a cocktail reception followed by a sumptuous dinner with wine and the presentation of awards.</p>
-
-<p align="left">Reserve your space to celebrate the outstanding achievements and excellence in the Canadian Health Informatics industry. To do so, please select from the following options:
-<!--click <a href="registration.php">here</a> to register.-->
-</p>
-
-<ul>
-<li><a href="register_table.php">Table(s) of 10</a> - $2,500 each plus HST</li>
-<li><a href="register_ind.php">Individual tickets (1 to 9 tickets)</a> - $255.00 each plus HST</li>
-</ul>
-
+<p align="left">Tickets are no longer available for this event.</p>
 
 </div>
 </td>
